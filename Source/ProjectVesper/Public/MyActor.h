77c8dc12d2558a2a0093d28a7cbcd8b303c77bb1
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-04-16 11:52:34 +0900

    JetBrainsRiderSetUpv2_DONE

---
+++ Source/ProjectVesper/Public/MyActor.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/PlayerInput.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class PROJECTVESPER_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int IntVar;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FInputActionKeyMapping TestIA;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
