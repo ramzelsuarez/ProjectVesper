c1a6193f6da9797f093f3bc15e6d139865db1ae1
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-04-20 22:30:43 +0900

    OnScreenDebugMessages_DONE

--- Source/ProjectVesper/Private/Item.cpp
+++ Source/ProjectVesper/Private/Item.cpp
@@ -17,6 +17,11 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 	
 	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Yellow, FString("Onscreen thingymajiggies!"));
+	}
 }
 
 // Called every frame
