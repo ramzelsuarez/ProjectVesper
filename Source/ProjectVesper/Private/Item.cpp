f1273c6ae50b30619a984e23804b00c8ffd053fa
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-04-20 23:14:15 +0900

    FormattingStringsForDebug_DONE

--- Source/ProjectVesper/Private/Item.cpp
+++ Source/ProjectVesper/Private/Item.cpp
@@ -29,5 +29,15 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	UE_LOG(LogTemp, Warning, TEXT("Delta tiiime: %f"), DeltaTime);
+
+	if (GEngine)
+	{
+		FString Name = GetName();
+		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Yellow, Message);
+
+		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
+	}
 }
 
