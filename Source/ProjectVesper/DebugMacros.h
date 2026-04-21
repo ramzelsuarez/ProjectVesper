5213f0a07b1d69a5a4d5a0349c11528d43ea6de8
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-04-21 19:58:17 +0900

    DebugMacros_SectionChallenge_DONE
    Created customizable debug macros.

--- Source/ProjectVesper/DebugMacros.h
+++ Source/ProjectVesper/DebugMacros.h
@@ -3,10 +3,10 @@
 
 
 #define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
-#define DRAW_LINE(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f);
-#define DRAW_POINT(Location) if (GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Red, true);
+#define DRAW_LINE(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Yellow, true, -1.f, 0, 1.f);
+#define DRAW_POINT(Location) if (GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Blue, true);
 #define DRAW_VECTOR(StartLocation, EndLocation) if (GetWorld()) \
 	{ \
-		DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, true, -1.f, 0, 1.f); \
+		DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Green, true, -1.f, 0, 1.f); \
 		DrawDebugPoint(GetWorld(), EndLocation, 15.f, FColor::Red, true); \
 	}
