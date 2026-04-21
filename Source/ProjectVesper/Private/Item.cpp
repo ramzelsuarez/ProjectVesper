7917022765a17b16aad3fb215123af3922405827
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-04-21 16:14:38 +0900

    DrawDebugLines_DONE

--- Source/ProjectVesper/Private/Item.cpp
+++ Source/ProjectVesper/Private/Item.cpp
@@ -20,39 +20,19 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play called!"));
-
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Yellow, FString("Onscreen thingymajiggies!"));
-	}
 
 	UWorld* World = GetWorld();
+	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
 
-	if (World)
-	{
-		FVector Location = GetActorLocation();
-		DRAW_SPHERE(Location)
-	}
 
-	
+	DRAW_SPHERE(Location)
+	DRAW_LINE(Location, Location + Forward * 100.f)
 }
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	UE_LOG(LogTemp, Warning, TEXT("Delta tiiime: %f"), DeltaTime);
-
-	if (GEngine)
-	{
-		FString Name = GetName();
-		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Yellow, Message);
-
-		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name);
-	}
 }
 
