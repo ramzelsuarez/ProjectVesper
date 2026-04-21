f31f7a963e81f888db9daa769de52f4185ad6c42
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-04-21 21:11:40 +0900

    GetActorWorldOffsetAndRotationC++_DONE

--- Source/ProjectVesper/DebugMacros.h
+++ Source/ProjectVesper/DebugMacros.h
@@ -3,10 +3,18 @@
 
 
 #define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
+#define DRAW_SPHERE_SingleFrame(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Blue, false, -1.f);
 #define DRAW_LINE(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Yellow, true, -1.f, 0, 1.f);
+#define DRAW_LINE_SingleFrame(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Yellow, false, -1.f, 0, 1.f);
 #define DRAW_POINT(Location) if (GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Blue, true);
+#define DRAW_POINT_SingleFrame(Location) if (GetWorld()) DrawDebugPoint(GetWorld(), Location, 15.f, FColor::Blue, false, -1.f);
 #define DRAW_VECTOR(StartLocation, EndLocation) if (GetWorld()) \
 	{ \
 		DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Green, true, -1.f, 0, 1.f); \
 		DrawDebugPoint(GetWorld(), EndLocation, 15.f, FColor::Red, true); \
+	}
+#define DRAW_VECTOR_SingleFrame(StartLocation, EndLocation) if (GetWorld()) \
+	{ \
+		DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Green, false, -1.f, 0, 1.f); \
+		DrawDebugPoint(GetWorld(), EndLocation, 15.f, FColor::Red, false, -1.f); \
 	}
