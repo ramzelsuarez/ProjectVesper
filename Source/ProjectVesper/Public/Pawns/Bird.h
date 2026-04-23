8513ec6b7805d6dd21c96bd51854bfcbfd79c25b
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-04-23 16:02:03 +0900

    SkeletalMeshComponentSetUp_C++_DONE
    + Animation asset for testing. After binding inputs using C++ I will be adding Animation BP on my birdieboy.

--- Source/ProjectVesper/Public/Pawns/Bird.h
+++ Source/ProjectVesper/Public/Pawns/Bird.h
@@ -7,6 +7,7 @@
 #include "Bird.generated.h"
 
 class UCapsuleComponent;
+class USkeletalMeshComponent;
 
 UCLASS()
 class PROJECTVESPER_API ABird : public APawn
@@ -26,4 +27,6 @@ private:
 	UPROPERTY(VisibleAnywhere)
 	UCapsuleComponent* Capsule;
 
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* BirdMesh;
 };
