6c1813abaae7ba012a4bc10a6c4757651a54c7de
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-04-25 02:03:15 +0900

    AddControllerInputC++_DONE

--- Source/ProjectVesper/Private/Pawns/Bird.cpp
+++ Source/ProjectVesper/Private/Pawns/Bird.cpp
@@ -69,6 +69,16 @@ void ABird::Move(const FInputActionValue& Value)
 	}
 }
 
+void ABird::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisValue = Value.Get<FVector2D>();
+	if (GetController())
+	{
+		AddControllerYawInput(LookAxisValue.X);
+		AddControllerPitchInput(LookAxisValue.Y);
+	}
+}
+
 void ABird::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
@@ -83,7 +93,7 @@ void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
 	{
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABird::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABird::Look);
 	}
-
 }
 
