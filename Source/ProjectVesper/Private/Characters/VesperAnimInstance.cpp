96fde99b078f54c862ac675821044261a5221829
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-04-28 02:03:27 +0900

    AnimInstance_C++_DONE

---
+++ Source/ProjectVesper/Private/Characters/VesperAnimInstance.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/VesperAnimInstance.h"
+#include "Characters/VesperCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UVesperAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	VesperCharacter = Cast<AVesperCharacter>(TryGetPawnOwner());
+	if (VesperCharacter)
+	{
+		VesperCharacterMovement = VesperCharacter->GetCharacterMovement();
+	}
+}
+
+void UVesperAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (VesperCharacterMovement)
+	{
+		GroundSpeed = UKismetMathLibrary::VSizeXY(VesperCharacterMovement->Velocity);
+	}
+}
---
+++ Source/ProjectVesper/Public/Characters/VesperAnimInstance.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "VesperAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTVESPER_API UVesperAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	class AVesperCharacter* VesperCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	class UCharacterMovementComponent* VesperCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed;
+};
