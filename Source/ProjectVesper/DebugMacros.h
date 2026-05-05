4109d7b0f73d0b776b7c93104049b49b005bef3a
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-05-06 02:24:43 +0900

    Interfaces_EnemyC++_DONE
    Created interfaces for Enemy which is both a character and Hit Result with C++.

--- Source/ProjectVesper/DebugMacros.h
+++ Source/ProjectVesper/DebugMacros.h
@@ -3,6 +3,7 @@
 
 
 #define DRAW_SPHERE(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Red, true);
+#define DRAW_SPHERE_COLOR(Location, Color) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 8.f, 12, Color, false, 5.f);
 #define DRAW_SPHERE_SingleFrame(Location) if (GetWorld()) DrawDebugSphere(GetWorld(), Location, 25.f, 12, FColor::Blue, false, -1.f);
 #define DRAW_LINE(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Yellow, true, -1.f, 0, 1.f);
 #define DRAW_LINE_SingleFrame(StartLocation, EndLocation) if (GetWorld()) DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Yellow, false, -1.f, 0, 1.f);
--- Source/ProjectVesper/Public/Enemy/Enemy.h
+++ Source/ProjectVesper/Public/Enemy/Enemy.h
@@ -4,10 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Interfaces/HitInterface.h"
 #include "Enemy.generated.h"
 
 UCLASS()
-class PROJECTVESPER_API AEnemy : public ACharacter
+class PROJECTVESPER_API AEnemy : public ACharacter, public IHitInterface
 {
 	GENERATED_BODY()
 
@@ -16,6 +17,8 @@ public:
 
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void GetHit(const FVector& ImpactPoint) override;
 protected:
 	virtual void BeginPlay() override;
 
