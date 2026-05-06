3e35bfe78956a1cc56aca352322995be12d48374
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-05-06 17:27:41 +0900

    DirectionalHitReactionC++_DONE
    Applied the correct hit reaction for sword hits to enemy.

--- Source/ProjectVesper/Private/Enemy/Enemy.cpp
+++ Source/ProjectVesper/Private/Enemy/Enemy.cpp
@@ -50,7 +50,6 @@ void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 void AEnemy::GetHit(const FVector& ImpactPoint)
 {
 	DRAW_SPHERE_COLOR(ImpactPoint, FColor::Orange);
-	PlayHitReactMontage(FName("FromLeft"));
 
 	const FVector Forward = GetActorForwardVector();
 	// Lower impact point to enemy's Actor Location Z
@@ -71,6 +70,24 @@ void AEnemy::GetHit(const FVector& ImpactPoint)
 	{
 		Theta *= -1.f;
 	}
+
+	FName Section("FromBack");
+
+	if (Theta >= -45.f && Theta < 45.f)
+	{
+		Section = FName("FromFront");
+	}
+	else if (Theta >= 45.f && Theta < 135.f)
+	{
+		Section = FName("FromRight");
+	}
+	else if (Theta >= -135.f && Theta < -45.f)
+	{
+		Section = FName("FromLeft");
+	}
+
+	PlayHitReactMontage(Section);
+
 	UKismetSystemLibrary::DrawDebugArrow(this, GetActorLocation(), GetActorLocation() + CrossProduct * 100.f, 5.f, FColor::Blue, 5.f);
 
 	if (GEngine)
