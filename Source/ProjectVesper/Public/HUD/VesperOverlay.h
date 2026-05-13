32306953780f5c50a296949e311f49e6b6a3f801
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-05-14 07:00:16 +0900

    UECrashVersionControlUpdate_DONE
    UE Crashed and I want to update my assets folder with additional fonts but crashed when i made a new C++ class -- probably because my RAM is already full and I had a lot of apps running. Updated git to select changes since I'm using my project in another school which has higher RAM space.

---
+++ Source/ProjectVesper/Public/HUD/VesperOverlay.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "VesperOverlay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTVESPER_API UVesperOverlay : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
