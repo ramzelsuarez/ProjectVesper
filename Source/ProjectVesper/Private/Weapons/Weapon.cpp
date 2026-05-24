ff2486476371d5a77975cdd30a75f8e2701138bf
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-05-25 00:39:01 +0900

    WeaponFixC++_DONE
    Weapon appeared funny for paladin when I played (maybe because I was still mid programming bugsyballs) so I made changes in Weapon.cpp. Will change after I'm done with that puny insect 🐶

--- Source/ProjectVesper/Private/Weapons/Weapon.cpp
+++ Source/ProjectVesper/Private/Weapons/Weapon.cpp
@@ -73,7 +73,10 @@ void AWeapon::PlayEquipSound()
 void AWeapon::AttachMeshToSocket(USceneComponent* InParent, const FName& InSocketName)
 {
 	FAttachmentTransformRules TransformRules(EAttachmentRule::SnapToTarget, true);
-	ItemMesh->AttachToComponent(InParent, TransformRules, InSocketName);
+
+	AttachToComponent(InParent, TransformRules, InSocketName);
+
+	// ItemMesh->AttachToComponent(InParent, TransformRules, InSocketName);
 }
 
 void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
